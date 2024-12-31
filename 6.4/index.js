2dcc8813505217e5a1106e367b7f511536092ba5
slavamuravey <slavamuravey@mail.ru>
2024-12-31 15:04:00 +0700

    Finish 6.4 task

---
+++ 6.4/index.js
@@ -0,0 +1,33 @@
+import { Readable } from 'node:stream';
+import { createServer } from 'node:http';
+
+const sleep = async (to) => new Promise(resolve => setTimeout(resolve, to));
+    
+async function* createGenerator() {
+    const chars = ['!', '@', '#', '$', '%', '^', '&', '*'];
+    let currentCharIdx = 0;
+    while (true) {
+        if (chars.length - 1 < ++currentCharIdx) {
+            currentCharIdx = 0;
+        }
+        yield '\x1b[2J\x1b[H';
+        await sleep(200);
+        yield `${chars[currentCharIdx]}\n`;
+    }
+}
+
+const server = createServer((req, res) => {
+    const chars = Readable.from(createGenerator());
+    res.statusCode = 200;
+    chars.pipe(res);
+});
+
+const PORT = 8000;
+
+server.listen(PORT, err => {
+    if (err) {
+        throw err;
+    }
+
+    console.log(`Server started listening on port ${PORT}`);
+});
